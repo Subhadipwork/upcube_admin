97aabd9a20b4fc951a55d621aad5b764ed40b3ad
Jay <jay@Jays-MacBook-Air.local>
2023-07-14 11:17:33 +0530

    update blade file

--- routes/web.php
+++ routes/web.php
@@ -16,9 +16,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('admin.layouts.layout');
+});
 
 Route::group(['prefix' => 'admin'], function () {
 
